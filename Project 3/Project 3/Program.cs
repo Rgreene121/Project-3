cc9cb95778ec786ae99c088d193cdd8321117fc3
rgreene121 <rabib.greene@gmail.com>
2017-08-09 14:39:13 -0400

    Department Store

---
+++ Project 3/Project 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Rabi and Amelia's ShoeStore");
+
+        }
+    }
+}
