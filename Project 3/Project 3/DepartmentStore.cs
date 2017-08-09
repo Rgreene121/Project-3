918209361f1a47f5b0a331b625a48d8dd898e821
rgreene121 <rabib.greene@gmail.com>
2017-08-09 15:19:47 -0400

    Changes

---
+++ Project 3/Project 3/DepartmentStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    class DepartmentStore
+    {
+            public string Shirts;
+            public string Shoes;
+            public string Pants;
+            public string Hats;
+
+        public virtual void PrintOptions()
+        {
+            Console.WriteLine($"Welcome to RAD, which department would you like to shop from {Shirts} or{Shoes} or {Pants} or {Hats}?");
+
+
+        }
+    }
+}
