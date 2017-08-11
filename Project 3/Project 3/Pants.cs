e82297965b80b3397e6a5b3b7efd9e2c97505171
CC4 STUDENT <mayfield9455@yahoo.com>
2017-08-10 21:53:17 -0400

    changed06

--- Project 3/Project 3/Pants.cs
+++ Project 3/Project 3/Pants.cs
@@ -82,72 +82,11 @@ namespace Project_3
         
     }
 
-    enum HatsStyle
-    {
-        Fedora = 1,
-        Beret,
-        Straw,
-        Cloche,
-        SchoolBoy,
-        Sun
-                        
-    }
-
-    class Hats
-    {
-        public void HatDepartment()
-        {
-
-            int HatsChoice;
-
-            Console.WriteLine("Which Hat style do you prefer \n 1.Fedora, \n 2.Beret, \n 3.Straw, \n 4.Cloche, \n 5.SchoolBoy, \n 6.Sun");
-
-            string Hats = Console.ReadLine();
-
-            int.TryParse(Hats, out HatsChoice);
-
-            switch ((HatsStyle)HatsChoice)
-            {
-                case HatsStyle.Fedora:
-                    Console.WriteLine("This hat says it all");
-                    break;
-
-                case HatsStyle.Beret:
-                    Console.WriteLine("This hat says I am important");
-                    break;
-
-                case HatsStyle.Straw:
-                    Console.WriteLine("Although this hat is durable it's also fasionable");
-                    break;
-
-                case HatsStyle.Cloche:
-                    Console.WriteLine("Classy Choice!");
-                    break;
-
-                case HatsStyle.SchoolBoy:
-                    Console.WriteLine("You have selected our hip yougster look");
-
-                    break;
-
-                case HatsStyle.Sun:
-                    Console.WriteLine("You have selected our hand crafted sun hats");
-                    break;
-
-                default:
-                    Console.WriteLine("Thank you for shopping with us today. Please come again");
-                    break;
-
-
-
-            }
-
-
-        }
-
-
-    }
+   
 
 
 
 }
 
+
+       
